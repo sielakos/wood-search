c0c7b146df2a39a82d5e1f35b0d57dc5d47bf2bb
sielakos@gmail.com <sielakos@gmail.com>
2014-09-05 15:35:41 +0200

    companies auth

--- search/companies_views.py
+++ search/companies_views.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, url_for, request
 from app import app
 from database import db
 from filters import replace_nl
+from auth import has_role_decorator, is_owner_decorator
 
 
 @app.route('/companies/')
@@ -12,6 +13,7 @@ def show_companies():
 
 
 @app.route('/companies/add', methods=['GET', 'POST'])
+@has_role_decorator('ROLE_ADMIN')
 def add_company():
     if request.method == 'POST':
         company = db.Company()
@@ -28,13 +30,17 @@ def add_company():
 def edit_company(company_id):
     company = db.Company.get_from_id(company_id)
 
-    if request.method == 'POST':
-        company.name = request.form['name']
-        company.description = replace_nl(request.form['description'])
-        company.save()
-        return redirect(url_for('show_company'))
+    @is_owner_decorator(company['_id'])
+    def action():
+        if request.method == 'POST':
+            company.name = request.form['name']
+            company.description = replace_nl(request.form['description'])
+            company.save()
+            return redirect(url_for('show_company'))
 
-    return render_template('company/edit_company.html', company=company)
+        return render_template('company/edit_company.html', company=company)
+
+    return action()
 
 
 @app.route('/companies/show/<ObjectId:company_id>')
@@ -46,6 +52,11 @@ def show_company(company_id):
 @app.route('/companies/remove/<ObjectId:company_id>', methods=['GET', 'POST'])
 def remove_company(company_id):
     company = db.Company.get_from_id(company_id)
-    company.remove_products()
-    company.delete()
-    return redirect(url_for('show_companies'))
+
+    @is_owner_decorator(company['_id'])
+    def action():
+        company.remove_products()
+        company.delete()
+        return redirect(url_for('show_companies'))
+
+    return action()
