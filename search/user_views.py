c07b025379e6465fbe0f7eb34d9013c158441944
sielakos@gmail.com <sielakos@gmail.com>
2014-09-06 09:12:27 +0200

    change password form

--- search/user_views.py
+++ search/user_views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from app import app
-from auth import has_role_decorator, logged_in_decorator, get_current_user
+from auth import has_role_decorator, logged_in_decorator, get_current_user, redirect_on_false_decorator, has_role
 from flask import render_template
+from database import db
 
 
 @app.route('/user/profile')
@@ -9,3 +10,15 @@ from flask import render_template
 def user_profile():
     user = get_current_user()
     return render_template('user/profile.html', user=user)
+
+
+@app.route('/user/change_password/<ObjectId:user_id>', methods=['GET', 'POST'])
+def user_change_password(user_id):
+    user = db.User.find_one_or_404({'_id': user_id})
+    current_user = get_current_user()
+
+    @redirect_on_false_decorator(user == current_user or has_role('ROLE_ADMIN', user=user))
+    def action():
+        return render_template('user/change_password.html', user=user)
+
+    return action()
